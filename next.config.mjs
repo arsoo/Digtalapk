0280449fdca8881d45a37f90cd15b21750fd7952
arsoo <arsoohunzai@gmail.com>
2026-04-08 21:07:56 +0500

    2nd commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
         // Only cache your own static images — not Next.js internals
         source: '/images/:path*',
         headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
+        env: ""
       },
     ];
   },
